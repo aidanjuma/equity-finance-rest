b87534c2ba2c50f16ea29c74a4f1ad0615e4a743
Aidan Juma <aidan@aidanjuma.dev>
2022-08-05 21:13:28 +0100

    🔔 add AssetData, change scraping dependencies
    - model AssetData: used for the scraped data from each asset page.
    - selenium required to render page before being scraped with parsel & webdriver manager also added to manage webdriver on local machine.

---
+++ equity/model/asset_data.py
@@ -0,0 +1,31 @@
+from marshmallow import Schema, fields
+
+
+class AssetData():
+    def __init__(self, ticker: str, market: str, google_finance_url: str, label: str, currency: dict, price: float, market_cap: int, price_range: dict, about: dict, news: dict) -> None:
+        self.ticker = ticker
+        self.market = market
+        self.google_finance_url = google_finance_url
+        self.label = label
+        self.currency = currency
+        self.price = price
+        self.market_cap = market_cap
+        self.price_range = price_range
+        self.about = about
+        self.news = news
+
+    def __repr__(self) -> str:
+        return '<AssetData(name={self.name!r})>'.format(self=self)
+
+
+class AssetDataSchema(Schema):
+    ticker = fields.Str()
+    market = fields.Str()
+    google_finance_url = fields.Url()
+    label = fields.Str()
+    currency = fields.Dict()
+    price = fields.Float()
+    market_cap = fields.Int()
+    price_range = fields.Dict()
+    about = fields.Dict()
+    news = fields.Dict()
