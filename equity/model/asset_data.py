2ccc08f02f9022cae9ecb9713415f3beb5b3041a
Aidan Juma <aidan@aidanjuma.dev>
2022-08-07 22:32:02 +0100

    💷 changed plan: ISO4217 in AssetData as string

--- equity/model/asset_data.py
+++ equity/model/asset_data.py
@@ -2,7 +2,7 @@ from marshmallow import Schema, fields
 
 
 class AssetData():
-    def __init__(self, ticker: str, market: str, google_finance_url: str, label: str, currency: dict, price: float, market_cap: int, price_range: dict, about: dict, news: dict) -> None:
+    def __init__(self, ticker: str, market: str, google_finance_url: str, label: str, currency: str, price: float, market_cap: int, price_range: dict, about: dict, news: dict) -> None:
         self.ticker = ticker
         self.market = market
         self.google_finance_url = google_finance_url
@@ -23,7 +23,7 @@ class AssetDataSchema(Schema):
     market = fields.Str()
     google_finance_url = fields.Url()
     label = fields.Str()
-    currency = fields.Dict()
+    currency = fields.Str()
     price = fields.Float()
     market_cap = fields.Int()
     price_range = fields.Dict()
