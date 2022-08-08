330ebe1bc8465ddcaa5cccfc5ab6ce179a602fb8
Aidan Juma <aidan@aidanjuma.dev>
2022-08-08 22:19:19 +0100

    📃 added dict of known market/currency pairings

---
+++ equity/provider/market_currencies.py
@@ -0,0 +1,57 @@
+# A dictionary pairing each market to its relevant currency.
+data = {
+    'NYSE': 'USD',  # USA
+    'NASDAQ': 'USD',  # USA
+    'TPE': 'TWD',  # Taiwan
+    'KPX': 'KRW',  # ROK (South Korea)
+    'BVMF': 'BRL',  # Brazil
+    'SHA': 'CNY',  # People's Republic of China (PRC)
+    'IDX': 'IDR',  # Indonesia
+    'WSE': 'PLN',  # Poland
+    'ETR': 'EUR',  # Germany
+    'IST': 'TRY',  # Turkey
+    'NZE': 'NZD',  # New Zealand
+    'LON': 'GBX',  # United Kingdom (exception to ISO4217)
+    'TYO': 'JPY',  # Japan
+    'HKG': 'HKD',  # Hong Kong SAR
+    'NSE': 'INR',  # India
+    'KLSE': 'MYR',  # Malaysia
+    'SHE': 'CNY',  # People's Republic of China (PRC)
+    'ASX': 'AUD',  # Australia
+    'BIT': 'EUR',  # Italy
+    'BKK': 'THB',  # Thailand
+    'TSE': 'CAD',  # Canada
+    'TADAWUL': 'SAR',  # Saudi Arabia
+    'TLV': 'ILA',  # Israel (exception to ISO4217)
+    'STO': 'SEK',  # Sweden
+    'CVE': 'CAD',  # Canada
+    'CPH': 'DKK',  # Denmark
+    'SGX': 'SGD',  # Singapore
+    'CNSX': 'CAD',  # Canada
+    'AMS': 'EUR',  # Netherlands
+    'FRA': 'EUR',  # Germany
+    'EPA': 'EUR',  # France
+    'SWX': 'CHF',  # Switzerland
+    'HEL': 'EUR',  # Finland
+    'KOSDAQ': 'KRW',  # ROK (South Korea)
+    'BOM': 'INR',  # India
+    'BME': 'EUR',  # Spain
+    'JSE': 'ZAC',  # South Africa (exception to ISO4217)
+    'BMV': 'MXN',  # Mexico
+    'BCBA': 'ARS',  # Argentina
+    'VIE': 'EUR',  # Austria
+    'EBR': 'EUR',  # Belgium
+    'ELI': 'EUR',  # Portugal
+    'ICE': 'ISK',  # Iceland
+    'TAL': 'EUR',  # Estonia
+    'VSE': 'EUR',  # Lithuania
+    'OTCMKTS': 'USD',  # USA
+    'NYSEARCA': 'USD',  # USA
+    'NYSEAMERICAN': 'USD',  # USA
+    'MUTF': 'USD',  # USA
+    'MUTF_IN': 'INR',  # India
+    'BATS': 'USD',  # USA
+    'CBOT': 'USD',  # USA
+    'CME_EMINIS': 'USD',  # USA
+    'NYMEX': 'USD'  # USA
+}
